be68e2a35336ec601afaf7c3782e1492b9779996
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2024-07-01 21:38:34 +0200

    remove unused function

--- web/server_test.go
+++ web/server_test.go
@@ -29,10 +29,6 @@ func (r *TestResponseRecorder) CloseNotify() <-chan bool {
 	return r.closeChannel
 }
 
-func (r *TestResponseRecorder) closeClient() {
-	r.closeChannel <- true
-}
-
 func NewTestResponseRecorder() *TestResponseRecorder {
 	return &TestResponseRecorder{
 		httptest.NewRecorder(),
