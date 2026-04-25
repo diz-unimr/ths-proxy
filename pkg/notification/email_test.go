01d933dab6f620c9f08fcd8bb16d8f611ea2e083
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2026-04-25 21:33:40 +0200

    test: notification send with attachment

--- pkg/notification/email_test.go
+++ pkg/notification/email_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/wneessen/go-mail"
 )
 
-func TestSend(t *testing.T) {
+func testSend(t *testing.T, doc *consent.Document) {
 	server := smtpmock.New(smtpmock.ConfigurationAttr{
 		LogToStdout:       true,
 		LogServerActivity: true,
@@ -37,13 +37,37 @@ func TestSend(t *testing.T) {
 	}
 	client := NewEmailClient(c)
 
-	client.Send("Oops, something went wrong", "TEST", "Body", nil)
+	client.Send("Oops, something went wrong", "TEST", "Body", doc)
 
 	messages := server.MessagesAndPurge()
 	assert.Len(t, messages, 2)
 
 	if err := server.Stop(); err != nil {
 		fmt.Println(err)
+	}
+}
+
+func TestSend(t *testing.T) {
+	cases := []struct {
+		name string
+		doc  *consent.Document
+	}{
+		{name: "WithoutAttachment", doc: nil},
+		{name: "WithAttachment", doc: createTestDocument()},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			testSend(t, c.doc)
+		})
+	}
+}
+
+func createTestDocument() *consent.Document {
+	return &consent.Document{
+		Name: "test",
+		Type: "application/pdf",
+		Data: new("JVBERi0xLjIgCjkgMCBvYmoKPDwKPj4Kc3RyZWFtCkJULyA5IFRmKFRlc3QpJyBFVAplbmRzdHJlYW0KZW5kb2JqCjQgMCBvYmoKPDwKL1R5cGUgL1BhZ2UKL1BhcmVudCA1IDAgUgovQ29udGVudHMgOSAwIFIKPj4KZW5kb2JqCjUgMCBvYmoKPDwKL0tpZHMgWzQgMCBSIF0KL0NvdW50IDEKL1R5cGUgL1BhZ2VzCi9NZWRpYUJveCBbIDAgMCA5OSA5IF0KPj4KZW5kb2JqCjMgMCBvYmoKPDwKL1BhZ2VzIDUgMCBSCi9UeXBlIC9DYXRhbG9nCj4+CmVuZG9iagp0cmFpbGVyCjw8Ci9Sb290IDMgMCBSCj4+CiUlRU9G"),
 	}
 }
 
